27ed8caef692f9ed58c2ee4e51de0e1eb6f3979e
poison <masahiro.fukuyama@roxx.co.jp>
2025-03-04 14:04:57 +0900

    testが動くところまで

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
-    coverage: {
-      reporter: ["text", "json", "html"],
+export default defineConfig(async () => {
+  // why esmのエラーが出るため遅延ロードを行う
+  const tsconfigPaths = (await import("vite-tsconfig-paths")).default;
+  return {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: "happy-dom",
+      setupFiles: "./vitest.setup.ts",
+      autoResetMocks: true,
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
     },
-  },
+  };
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,7 @@
-import "@testing-library/jest-dom/vitest";
+import { expect } from "vitest";
+import * as matchers from "vitest-axe/matchers";
+
+import "vitest-axe/extend-expect";
+import "vitest-canvas-mock";
+
+expect.extend(matchers);
